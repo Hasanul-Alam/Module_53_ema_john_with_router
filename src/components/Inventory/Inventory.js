cc60334efdc5974c47e9595b723ef787ad3c2b33
Hasanul Alam Prince <74726194+Hasanul-Alam@users.noreply.github.com>
2021-10-02 15:48:29 +0600

    Changed to my repository

---
+++ src/components/Inventory/Inventory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Inventory = () => {
+    return (
+        <div>
+            <h2>This is just an inventory page.</h2>
+        </div>
+    );
+};
+
+export default Inventory;
